80a3a04ee7de39044b88acfb74730bdc57c10903
Fabi <fabian.reichenberg@outlook.de>
2022-02-15 08:14:49 +0100

    added firebase

--- talkaboat/src/app/app.module.ts
+++ talkaboat/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService } from '@angular/fire/analytics';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- talkaboat/src/environments/environment.prod.ts
+++ talkaboat/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'enodi-bc',
+    appId: '1:1063546575410:web:8a4a8f4daa8ce38c78fa2b',
+    storageBucket: 'enodi-bc.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAgOXyWLxAVxG2FmsJkXGCEzfzBpfkj1dA',
+    authDomain: 'enodi-bc.firebaseapp.com',
+    messagingSenderId: '1063546575410',
+    measurementId: 'G-28XR7FMP0S',
+  },
   production: true
 };
