6bdec73f9ee4c7aa221a211147445caee71567ea
AriPapari <ari.gecaj@gmx.de>
2022-04-18 21:06:01 +0200

    adds content to episode detail

--- src/app/routing-components/search/episode-detail/episode-detail.component.ts
+++ src/app/routing-components/search/episode-detail/episode-detail.component.ts
@@ -1,6 +1,8 @@
+import { Playlist } from './../../../services/repository/search-repository/models/playlist/playlist.model.dto';
 import { Component, OnInit } from '@angular/core';
 import { listAnimation, listItemAnimation } from 'src/app/animations';
 import { WebsiteStateService } from 'src/app/services/website-state/website-state.service';
+import { PLAYLIST_ARRAY_MOCK } from 'src/constants/mocks/playlist.mock.constants';
 
 @Component({
   selector: 'app-episode-detail',
@@ -11,6 +13,7 @@ import { WebsiteStateService } from 'src/app/services/website-state/website-stat
 export class EpisodeDetailComponent implements OnInit {
 
   canNavigateBack: boolean = false;
+  playlistMock: Playlist = PLAYLIST_ARRAY_MOCK[0]
 
   constructor(private readonly websiteStateService: WebsiteStateService) { }
 
@@ -20,6 +23,10 @@ export class EpisodeDetailComponent implements OnInit {
 
   backNavigation() {
     this.websiteStateService.backNavigation();
+  }
+
+  convertTime(seconds: number): string {
+    return new Date(seconds * 1000).toISOString().substr(11, 8)
   }
 
 }
