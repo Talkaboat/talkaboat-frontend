590d195d0b175eae16628923812d157e2a4e8d64
Fabi <fabian.reichenberg@outlook.de>
2022-05-20 14:48:33 +0200

    mini bugfix

--- src/app/pipes/time/time.pipe.ts
+++ src/app/pipes/time/time.pipe.ts
@@ -10,7 +10,8 @@ export class TimePipe implements PipeTransform {
   transform(timestamp: number | undefined, type: string): string {
     if (timestamp) {
       switch (type) {
-        case 'hours': return new Date(timestamp * 1000).toISOString().substring(11, 19);
+        case 'hours': return new Date(timestamp * 1000).toISOString().substring(11, 16);
+        case 'minutes': return new Date(timestamp * 1000).toISOString().substring(14, 19);
         case 'date': return MathUtils.timeConverter(timestamp, 0, false).substring(0, 11);
       }
       return timestamp.toString();
