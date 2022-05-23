f16cc37d66ab9096cb8eafbe4551b6457ffc1c3e
Fabi <fabian.reichenberg@outlook.de>
2022-05-23 16:30:33 +0200

    token claim modal styling

--- src/app/static-components/token-claim/token-claim.component.ts
+++ src/app/static-components/token-claim/token-claim.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-token-claim',
@@ -7,13 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TokenClaimComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("tokenClaimCheckbox") tokenClaimCheckbox: ElementRef | null = null;
+  vestedTokens = 0;
+  totalTokens = 0;
+  claimMode = -1;
+  claimAmount = new FormControl('', [Validators.required, Validators.maxLength(24)]);
+  constructor(private readonly userService: UserService) { }
 
   ngOnInit(): void {
+    this.updateTokens();
+    this.userService.onRewardsChanged.subscribe(_ => this.updateTokens());
+
+  }
+
+  setToMaxClaimableAmount() {
+    this.claimMode = -1;
+    this.claimAmount.setValue(this.vestedTokens);
   }
 
   claimAboat() {
-
+    this.claimMode = 1;
+    this.claimAmount.disable();
   }
 
+  claimBnb() {
+    this.claimMode = 2;
+    this.claimAmount.disable();
+  }
+
+  updateTokens() {
+    this.totalTokens = this.userService.currentRewards.total;
+    this.vestedTokens = this.userService.currentRewards.vested;
+  }
+
+  confirmClaim() {
+    this.claimAmount.enable();
+  }
 }
